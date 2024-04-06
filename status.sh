e9a12ff23924ec3e0037bb59983dcb3739b5d525
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-06 19:33:13 +0200

    added xclip to status

--- status.sh
+++ status.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+if ! command -v docker &> /dev/null
+then
+    echo "Error: docker could not be found"
+    exit 1
+fi
+
+if ! xclip -version &> /dev/null
+then
+    echo "Warning: xclip could not be found"
+    echo "Note: xclip is used to copy the suggested command to clipboard"
+fi
+
 if ! command -v kubelet &> /dev/null
 then
-    echo "kubelet could not be found"
+    echo "Error: kubelet could not be found"
     exit 1
 fi
 
 if ! systemctl is-active --quiet kubelet
 then
-    echo "kubelet is not running"
+    echo "Error: kubelet is not running"
+    echo " -> start with: sudo systemctl start kubelet"
+    xclip -sel clip <<< "sudo systemctl start kubelet"
     exit 1
 fi
 
 if ! command -v kubectl &> /dev/null
 then
-    echo "kubectl could not be found"
+    echo "Error: kubectl could not be found"
     exit 1
 fi
 
